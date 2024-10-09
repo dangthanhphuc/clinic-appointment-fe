f7511bc5c6447fa3662fbf9c2acb2f401fc72fb9
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2024-10-09 23:14:31 +0700

    Completed header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserResponse } from '../../responses/user.response';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { CommonModule } from '@angular/common';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faGear} from '@fortawesome/free-solid-svg-icons'
 
 
 @Component({
@@ -13,7 +14,8 @@ import { faBell } from '@fortawesome/free-regular-svg-icons';
   standalone: true,
   imports: [
     CommonModule,
-    FontAwesomeModule
+    RouterModule,
+    FontAwesomeModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -21,6 +23,7 @@ import { faBell } from '@fortawesome/free-regular-svg-icons';
 export class HeaderComponent implements OnInit{
   // Icons
   faBell = faBell;
+  faGear = faGear;
   
   userResponse!: UserResponse;
 
@@ -28,6 +31,7 @@ export class HeaderComponent implements OnInit{
     private localStorageService : LocalStorageService,
     private router : Router
   ) {
+    
     const user = localStorageService.getValueFromLocalStorage("user");
 
   }
